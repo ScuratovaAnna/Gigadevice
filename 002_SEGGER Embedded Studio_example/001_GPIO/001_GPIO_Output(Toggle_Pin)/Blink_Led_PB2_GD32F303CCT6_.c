a0a0757179e8da4de4028d1cd1863fc7bc958a9a
unknown <pivnev_tosno@mail.ru>
2025-06-02 15:39:43 +0300

    new repository

--- 002_SEGGER Embedded Studio_example/001_GPIO/001_GPIO_Output(Toggle_Pin)/Blink_Led_PB2_GD32F303CCT6_.c
+++ 002_SEGGER Embedded Studio_example/001_GPIO/001_GPIO_Output(Toggle_Pin)/Blink_Led_PB2_GD32F303CCT6_.c
@@ -1,4 +1,4 @@
-/**-----------------------------------------------------------------------
+/**-------------------------------------------------------------------
  \date  16.10.2024
  *
  | ** Первый пример поморгаем светодиодами.            ** |
@@ -39,4 +39,68 @@ int main(void)
     }
 }
 
+/*************************** End of file ****************************/
+/**-------------------------------------------------------------------
+ \date  02.06.2025
+ *
+ | ** Поморгаем двумя светодиодами.            ** |
+ | ** Код написан в IDE SEGGER Embedded Studio for ARM ** |
+ *   BluePill+
+ *   GD32F303CCT6   
+ *   ------------ 
+ *  |            |
+ *  |            |
+ *  |            |
+ *  |        PB.6| ---->  LED
+ *  |       PB.13| ---->  LED
+ *  |            |
+ *  |            |
+ *  |      +3.3V |
+ *  |        GND | 
+ */
+/************************** The second example **********************/
+#include "gd32f30x.h"
+#include "systick.h"
+
+#define SET_BIT(REG, BIT) ((REG) |= (BIT))
+#define CLEAR_BIT(REG, BIT) ((REG) &= ~(BIT))
+#define READ_BIT(REG, BIT) ((REG) & (BIT))
+#define CLEAR_REG(REG) ((REG) = (0x0))
+#define WRITE_REG(REG, VAL) ((REG) = (VAL))
+#define READ_REG(REG) ((REG))
+#define MODIFY_REG(REG, CLEARMASK, SETMASK) WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#define MD6   0x3U << 24
+#define CTL6  0x0U << 26
+
+#define CTL13 0x0U << 22
+#define MD13  0x3U << 20
+
+int main(void) {
+  systick_config();
+  gpio_deinit(GPIOB);
+  /* enable the peripherals clock */
+  // rcu_periph_clock_enable(RCU_GPIOB);
+  SET_BIT(RCU_APB2EN, RCU_APB2EN_PBEN);
+  /* GPIOB output */
+  // gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
+  MODIFY_REG(GPIO_CTL0(GPIOB), GPIO_CTL0_MD6,  MD6);
+  MODIFY_REG(GPIO_CTL0(GPIOB), GPIO_CTL0_CTL6, CTL6);
+  // gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
+  MODIFY_REG(GPIO_CTL1(GPIOB), GPIO_CTL1_MD13,  MD13);
+  MODIFY_REG(GPIO_CTL1(GPIOB), GPIO_CTL1_CTL13, CTL13);
+
+  while (1) {
+    SET_BIT(GPIO_BOP(GPIOB), GPIO_BOP_BOP6);
+    SET_BIT(GPIO_BC(GPIOB), GPIO_BC_CR13);
+    // gpio_bit_set(GPIOB, GPIO_PIN_0);
+    // GPIO_BOP(GPIOB) |= (1 << 0);
+    delay_1ms(100);
+    // gpio_bit_reset(GPIOB, GPIO_PIN_0);
+    // GPIO_BC(GPIOB)  |= (1 << 0);
+    SET_BIT(GPIO_BC(GPIOB), GPIO_BC_CR6);
+    SET_BIT(GPIO_BOP(GPIOB), GPIO_BOP_BOP13);
+    delay_1ms(100);
+  }
+}
 /*************************** End of file ****************************/
--- 002_SEGGER Embedded Studio_example/001_GPIO/002_GPIO_Input(Read_pin)/Button_Led_GD32F303CCT6_.c
+++ 002_SEGGER Embedded Studio_example/001_GPIO/002_GPIO_Input(Read_pin)/Button_Led_GD32F303CCT6_.c
@@ -43,4 +43,66 @@ int main(void) {
   }
 }
 /*************************** End of file ****************************/
+/**-------------------------------------------------------------------
+ \date  02.06.2025
+ *
+ | ** Поморгаем двумя светодиодами.            ** |
+ | ** Код написан в IDE SEGGER Embedded Studio for ARM ** |
+ *   BluePill+
+ *   GD32F303CCT6   
+ *   ------------ 
+ *  |            |
+ *  |            |
+ *  |            |
+ *  |        PB.3| ---->  LED
+ *  |            |
+ *  |      PA.10 | <----  Button
+ *  |            |
+ *  |      +3.3V |
+ *  |        GND | 
+ */
+/************************** The second example **********************/
+#include "gd32f30x.h"
+#include "stdbool.h"
+#include "systick.h"
 
+#define SET_BIT(REG, BIT) ((REG) |= (BIT))
+#define CLEAR_BIT(REG, BIT) ((REG) &= ~(BIT))
+#define READ_BIT(REG, BIT) ((REG) & (BIT))
+#define CLEAR_REG(REG) ((REG) = (0x0))
+#define WRITE_REG(REG, VAL) ((REG) = (VAL))
+#define READ_REG(REG) ((REG))
+#define MODIFY_REG(REG, CLEARMASK, SETMASK) WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#define MD3   0x3U << 12
+#define CTL3  0x0U << 14
+
+#define MD10  0b00 << 8
+#define CTL10 0b01 << 10
+
+volatile bool condition = false;
+
+
+int main(void) {
+ systick_config();
+  // rcu_periph_clock_enable(RCU_GPIOA);
+  SET_BIT(RCU_APB2EN, RCU_APB2EN_PAEN);
+  //rcu_periph_clock_enable(RCU_GPIOB);
+  SET_BIT(RCU_APB2EN, RCU_APB2EN_PBEN);
+
+  //gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ, GPIO_PIN_3);
+  MODIFY_REG(GPIO_CTL0(GPIOB), GPIO_CTL0_MD3,  MD3);
+  MODIFY_REG(GPIO_CTL0(GPIOB), GPIO_CTL0_CTL3, CTL3);
+
+  //gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_2MHZ, GPIO_PIN_10);
+  MODIFY_REG(GPIO_CTL1(GPIOA), GPIO_CTL1_MD10,  MD10);
+  MODIFY_REG(GPIO_CTL1(GPIOA), GPIO_CTL1_CTL10, CTL10);
+
+   while (1) {
+  //condition = gpio_input_bit_get(GPIOA, GPIO_PIN_10);
+  condition = READ_BIT(GPIO_ISTAT(GPIOA), GPIO_PIN_10);
+  //(condition == 0x0u) ? (gpio_bit_set(GPIOB, GPIO_PIN_3)) : (gpio_bit_reset(GPIOB, GPIO_PIN_3));
+  (condition == false) ? (SET_BIT(GPIO_BOP(GPIOB), GPIO_BOP_BOP3)) : (SET_BIT(GPIO_BC(GPIOB), GPIO_BC_CR3));
+  }
+}
+/*************************** End of file ****************************/
